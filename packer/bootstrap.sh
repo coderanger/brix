6cbfdb8fdbd2c653fb2094bff0fe37b90c43bee4
Noah Kantrowitz <noah@coderanger.net>
2014-02-11 15:07:13 -0800

    More on init and add a bootstrap script.

---
+++ packer/bootstrap.sh
@@ -0,0 +1,37 @@
+#!/bin/bash -xe
+#
+# Author:: Noah Kantrowitz <noah@coderanger.net>
+#
+# Copyright 2014, Balanced, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Config options
+TAG="$1"
+ENV="$2"
+ROLE="$3"
+
+# Set the hostname
+LOCAL_IPV4="$(curl http://169.254.169.254/latest/meta-data/local-ipv4)"
+HOSTNAME="${TAG}-${ENV}-$(echo $LOCAL_IPV4 | sed s/\\./-/g)"
+echo "$HOSTNAME" > /etc/hostname
+hostname "$HOSTNAME"
+
+# Write Chef first-boot JSON
+cat > /etc/chef/first-boot.json <<EOP
+{"run_list":["recipe[role-base]", "recipe["$ROLE"]}
+EOP
+
+# Run Chef
+chef-client --environment "$ENV" --json-attributes /etc/chef/first-boot.json
--- packer/init.sh
+++ packer/init.sh
@@ -1,4 +1,21 @@
 #!/bin/bash -xe
+#
+# Author:: Noah Kantrowitz <noah@coderanger.net>
+#
+# Copyright 2014, Balanced, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 
 # Install Chef
 curl -L https://www.opscode.com/chef/install.sh | sudo bash
@@ -27,3 +44,14 @@ sudo mv /tmp/validation.pem /tmp/client.rb /etc/chef
 sudo chown -R root:root /etc/chef
 sudo chmod 700 /etc/chef
 sudo chmod 600 /etc/chef/validation.pem /etc/chef/client.rb
+
+# Ohai hints
+sudo mkdir /etc/chef/ohai /etc/chef/ohai/hints
+echo '{}' | sudo tee /etc/chef/ohai/hints/ec2.json
+sudo chmod 700 /etc/chef/ohai /etc/chef/ohai/hints
+sudo chmod 600 /etc/chef/ohai/hints/ec2.json
+
+# Move the bootstrap script into place
+mv /tmp/bootstrap.sh /opt
+chown root:root /opt/bootstrap.sh
+chmod 744 /opt/bootstrap.sh
