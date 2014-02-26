8d33da9a733b17f2d1bc5cd4bcc95956da0e13e9
Noah Kantrowitz <noah@coderanger.net>
2014-02-25 18:53:43 -0800

    Balanced API template.

---
+++ stacks/balanced_api.py
@@ -0,0 +1,30 @@
+#
+# Author:: Noah Kantrowitz <noah@coderanger.net>
+#
+# Copyright 2014, Balanced, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+from .base import AppTemplate
+
+
+class BalancedApi(AppTemplate):
+    """Balanced API service"""
+
+    CHEF_RECIPE = 'role-balanced-api'
+    STACK_TAG = 'bapi'
+
+
+if __name__ == '__main__':
+    print(BalancedDocs().to_json())
